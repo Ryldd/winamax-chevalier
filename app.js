6fa97c93295253164058f195c9f685d3647c56da
Ryldd <nathan.dias3119@gmail.com>
2022-11-26 00:05:40 +0100

    fix cron

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
 });
 
 // Affichage des matchs du jour
-cron.schedule('01 23 * * *', async function (){
+cron.schedule('01 0 * * *', async function (){
     await showMatches(await winamaxChevalier.dayMatches(), null);
 })
 
